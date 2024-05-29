a0da280d36c8e25be5d13d1bdd0fffd4d1e3160a
HugoSalvador <hugossalvador1241@gmail.com>
2024-05-29 17:39:16 -0300

    Segundo commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Details } from '../src/pages/Details';
 
-
+//teste
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Details />
