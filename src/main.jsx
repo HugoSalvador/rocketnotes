6cd21107f88aef6ef57924b643a068335f68b180
HugoSalvador <hugossalvador1241@gmail.com>
2024-06-04 14:02:48 -0300

    Componente de tags e page da home

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Details } from './pages/Details';
 import { ThemeProvider } from 'styled-components';
 
-
 import GlobalStyles from './styles/global';
+
 import theme from './styles/theme';
 
+import { Home } from './pages/Home';
 
-//teste
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,21 +1,52 @@
-import { Container } from './styles'
+import { Container, Links, Content } from './styles'
 
+import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
+import { ButtonText  } from '../../components/ButtonText';
 
 export function Details() {
 
 
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Hugo Salvador</span>
+        <Header />
 
+        <main>
+          <Content>     
+        
+          <ButtonText title="Excluir nota" />
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <Button title="Entrar" />
-      <Button title="Cadastrar"/>
-      <Button title="Voltar"/>
-      
+          <p>
+            Lorem ipsum sit damet lorem ipsum 
+            sit damet lorem ipsum sit damet lorem 
+            ipsum sit damet lorem ipsum sit damet lorem 
+            ipsum sit damet lorem ipsum 
+            sit damet lorem ipsum 
+            sit damet lorem ipsum sit damet
+          </p>
 
+          <Section title="Links Úteis">
+            <Links>
+              <li><a href="#">https://app.rocketseat.com.br/</a></li>
+              <li><a href="#">https://app.rocketseat.com.br/</a></li>
+
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          
+          </Section>
+
+          <Button title="Voltar"/>
+          </Content >
+        </main>
 
     </Container>
   )
